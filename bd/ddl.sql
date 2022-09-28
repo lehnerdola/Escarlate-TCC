624b9d48aa7686fc5badd5412aec6a837a1d4461
Leticia <ln9458143@gmail.com>
2022-09-28 17:21:31 -0300

    not null

--- bd/ddl.sql
+++ bd/ddl.sql
@@ -9,9 +9,9 @@ ds_senha varchar(50) not null
 
 create table tb_artista(
 id_artista int primary key auto_increment,
-nm_artista varchar(150) not null,
-ds_artista varchar(150) not null,
-img_artista varchar(700) not null
+nm_artista varchar(150) ,
+ds_artista varchar(150) ,
+img_artista varchar(700) 
 );
 
 create table tb_artista_categoria(
@@ -31,14 +31,16 @@ ds_categoria varchar(150) not null
 create table tb_produto(
 id_produto int primary key auto_increment,
 id_artista int,
-nm_produto varchar(150) not null,
-ds_tam varchar(20) not null,
-bt_disponivel bool not null,
-vl_preco varchar(50) not null,
-qtd_produto int not null,
-img_produto varchar(700) not null,
+nm_produto varchar(150) ,
+ds_tam varchar(20) ,
+bt_disponivel bool,
+vl_preco varchar(50) ,
+qtd_produto int ,
+img_produto varchar(700),
 foreign key (id_artista) references tb_artista(id_artista)
 );
+
+drop table tb_artista;
 
 create table tb_produto_categoria(
 id_produto_categoria int primary key auto_increment,
@@ -57,20 +59,20 @@ nm_categoria varchar(150)
 );
 create table tb_usuario(
 id_usuario int primary key auto_increment,
-nm_usuario varchar(150) not null,
-ds_email varchar(150) not null,
-ds_senha varchar(25) not null,
-ds_cpf_usuario varchar(50) not null,
+nm_usuario varchar(150) ,
+ds_email varchar(150) ,
+ds_senha varchar(25) ,
+ds_cpf_usuario varchar(50) ,
 ds_telefone varchar(50)
 );
 
 create table tb_usuario_endereco(
 id_usuario_end int primary key auto_increment,
 id_usuario int,
-ds_logradouro varchar(150) not null,
+ds_logradouro varchar(150) ,
 ds_complemento varchar(150),
-nr_endereco int not null,
-nr_cep int not null,
+nr_endereco int ,
+nr_cep int ,
 
 foreign key (id_usuario) references tb_usuario(id_usuario)
 );
@@ -101,9 +103,9 @@ foreign key (id_produto) references tb_produto(id_produto)
 create table tb_pag_cartao(
 id_pag_cartao int primary key auto_increment,
 id_pedido int,
-nm_cartao varchar(150) not null,
-nr_cartao int not null,
-cvv_cartao int not null,
+nm_cartao varchar(150) ,
+nr_cartao int ,
+cvv_cartao int,
 dt_vencimento date,
 
 foreign key (id_pedido) references tb_pedido(id_pedido)
