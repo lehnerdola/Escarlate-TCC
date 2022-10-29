223fbfaafceb23e519f74c776476e0d7824f6b00
XhScarlet <gabecs0000@gmail.com>
2022-10-29 18:20:17 -0300

    ajustes

--- site/src/pages/ADM/Cliente/index.js
+++ site/src/pages/ADM/Cliente/index.js
@@ -1,7 +1,19 @@
-import MenuADM from '../../Components/Adm/menu/index'
-import './index.scss'
+import MenuADM from '../../Components/Adm/menu/index';
+import './index.scss';
+import { useState, useEffect } from "react";
 
 export default function Clientes(){
+    const [ clientes, setClientes ] = useState([]);
+
+    async function carregarClientes(){
+        const carregarClientes = await listarTodosClientes();
+        setClientes(carregarClientes);
+    }
+
+    useEffect(() => {
+        carregarClientes();
+    }, [])
+
     return(
         <main>
         <MenuADM/>
@@ -10,53 +22,20 @@ export default function Clientes(){
             <h1>Clientes</h1>
             <section className='align-caixas'>            
                 <div className='caixa-clientes'>
-                <img src='../../../assets/images/Queen.jpg' width={80} style={{borderRadius:"50px"}}/>
-                <div>
-                <p>Ana Júlia</p>
-                <p>naju@gmail.com</p>
-                <p>SP, Avenida Paulista</p>
-                </div>
-                <div>
-                <p>Compras feitas</p>
-                <p>2(total)</p>
-                </div>
-                
-            </div>
-             <div className='caixa-clientes'>
-                <img src='../../../assets/images/Queen.jpg' width={80} style={{borderRadius:"50px"}}/>
-                <div>
-                <p>Ana Júlia</p>
-                <p>naju@gmail.com</p>
-                <p>SP, Avenida Paulista</p>
-                </div>
-                <div>
-                <p>Compras feitas</p>
-                <p>2(total)</p>
-                </div>
-            </div>
-            <div className='caixa-clientes'>
-                <img src='../../../assets/images/Queen.jpg' width={80} style={{borderRadius:"50px"}}/>
-                <div>
-                <p>Ana Júlia</p>
-                <p>naju@gmail.com</p>
-                <p>SP, Avenida Paulista</p>
-                </div>
-                <div>
-                <p>Compras feitas</p>
-                <p>2(total)</p>
-                </div>
-            </div>
-            <div className='caixa-clientes'>
-                <img src='../../../assets/images/Queen.jpg' width={80} style={{borderRadius:"50px"}}/>
-                <div>
-                <p>Ana Júlia</p>
-                <p>naju@gmail.com</p>
-                <p>SP, Avenida Paulista</p>
-                </div>
-                <div>
-                <p>Compras feitas</p>
-                <p>2(total)</p>
-                </div>
+                    {clientes.map (item =>
+                          <div>
+                            <img src={'../../../assets/images/cliente-padrao.png'} width={80}/>
+                            <p> {item.nome} </p>
+                            <p> {item.email} </p>
+                            <img src={'../../../assets/images/lumi.png'} width={20}/>
+                            <img src={'../../../assets/images/list.png'} width={20}/>
+                            <span> 
+                                <img src={'../../../assets/images/map'} width={20}/>
+                                <p> {item.endereco} </p> 
+                            </span>
+                            <p> compras feitas {item.total} </p>
+                          </div>        
+                    )}
             </div>
             </section>
             </section>
